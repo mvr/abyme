f8efb6ce7e237ed898ff98dca0548edc8a55e314
Mitchell Riley <mitchell.v.riley@gmail.com>
2016-10-27 20:16:40 -0400

    Initial pass at a proper gameplay view

---
+++ Director.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+var origin_block = null
+var blocks = {}
+
+func _ready():
+	pass
+
+func find_island():
+	var found = {self.origin_block : Vector2(0, 0)}
+	var to_search = found
+
+	while not to_search.empty():
+		var next_search = {}
+		for b in to_search:
+			var as = b.adjacent_blocks_with_displacement()
+			for a in as.keys():
+				if not found.has(b):
+					var bpos = to_search[b] + as[a]
+					next_search[b] = bpos
+					found[b] = bpos
+		to_search = next_search
+
+	self.blocks = found
+
+func arrange():
+	find_island()
+
+func _draw():
+	for b in blocks:
+		var grid_pos = blocks[b]
+
+		var block_size = b.size * b.tilemap.get_cell_size()
+
+		var pos = grid_pos * block_size
+		var drawrect = Rect2(pos, block_size)
+
+		var texture = b.viewport.get_render_target_texture()
+		self.draw_texture_rect(texture, drawrect, false)
--- Level.gd
+++ Level.gd
@@ -1,10 +1,25 @@
 extends Node2D
 
 var blocks = []
+var player_block = null
 
 func _ready():
-	var blocks = get_children()
+	var blocks = get_tree().get_nodes_in_group("blocks")
+
 	for b in blocks:
 		b.find_parent_block()
+
 	for b in blocks:
 		b.set_child_blocks()
+
+	for b in blocks:
+		if b.is_player:
+			self.player_block = b
+			break
+
+	arrange()
+
+func arrange():
+	var director = get_node("Director")
+	director.origin_block = self.player_block.parent_block
+	director.arrange()
