0ab2e161346d2ed333f9794054c57e981b3091bb
Mitchell Riley <mitchell.v.riley@gmail.com>
2016-12-01 01:08:50 -0500

    Change player highlight colour

--- Constants.gd
+++ Constants.gd
@@ -6,7 +6,7 @@ var block_size = 5
 
 # Drawing
 var background_fade = Color(0.5, 0.5, 0.5, 0.5)
-var player_highlight = Color(0.5, 0, 0)
+var player_highlight = Color(0.7, 0.1, 0.1)
 
 # Animation
 
