36d8c0f25310c604de1557d9eb9c8abc2f0c75a2
Mitchell Riley <mitchell.v.riley@gmail.com>
2017-07-28 23:47:30 -0400

    Some more partial work on generator

--- src/Abyme/Universe/Generator.hs
+++ src/Abyme/Universe/Generator.hs
@@ -2,9 +2,11 @@
 module Abyme.Universe.Generator where
 
 import qualified Data.Map.Strict as M
+import Control.Monad (guard)
 import Control.Lens
 import Linear
 
+import Abyme.Direction
 import Abyme.Polyomino
 import Abyme.Universe
 import Abyme.Addressing
@@ -20,9 +22,9 @@ minimal = Universe (M.fromList [(id1, region1), (id2, region2)])
         region1 = Region id1 id2 (V2 0 0) [monomino]
         region2 = Region id2 id1 (V2 0 0) [monomino]
 
--- Needs a "fake square" off the edge of a Shape
-growPiece :: Universe -> Square -> Universe
-growPiece u (Square p l) = u & atPiece p . shapePolyomino . polyominoSquares %~ (l:)
+-- Assuming there isn't already a square there
+growPiece :: Universe -> Piece -> V2 Integer -> Universe
+growPiece u p n = u & atPiece p . shapePolyomino . polyominoSquares %~ (n:)
 
 locationPositionOnParent :: Location -> V2 Integer
 locationPositionOnParent (Location (Square _ p) subp) = levelScale *^ p + subp
@@ -33,3 +35,13 @@ growChild u l = fuseInhabitantRegions (u & universeRegions . at newId ?~ newRegi
   where newId = newRegionId u
         locationRegion = l ^. locationSquare . squarePiece . pieceRegion
         newRegion = Region newId (locationRegion ^. regionId) (locationPositionOnParent l) [monomino]
+
+potentialNewSquares :: Universe -> Region -> [(Piece, V2 Integer)]
+potentialNewSquares u r = do
+  p <- regionPieces r
+  l <- halo u p
+  guard (not $ isInhabited u l)
+  return (p, locationPositionOnParent l)
+
+potentialNewChildren :: Universe -> Region -> [Location]
+potentialNewChildren u r = filter (not . isInhabited u) $ constituentLocations u r
