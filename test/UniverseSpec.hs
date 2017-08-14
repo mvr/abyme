99ee6a70a78094258edebeec79dceedc425d1d30
Mitchell Riley <mitchell.v.riley@gmail.com>
2017-08-13 23:17:52 -0400

    Print counterexamples when properties fail

--- test/UniverseSpec.hs
+++ test/UniverseSpec.hs
@@ -2,6 +2,7 @@ module UniverseSpec (spec) where
 
 import Test.Hspec
 import Test.Hspec.QuickCheck
+import Test.QuickCheck.Property
 
 import Abyme.Universe
 import Abyme.Universe.Generate
@@ -14,11 +15,11 @@ spec = do
   describe "addressing" $ do
     prop "square occupies its location" $ \u -> do
       s <- randomSquare u
-      return $ Just s == inhabitant u (squareLocation u s)
+      return $ Just s === inhabitant u (squareLocation u s)
 
   describe "splitting" $ do
     prop "splitting then unsplitting is identity" $ \u -> do
       s <- randomPiece u
       let c = findChunk u s
       let (r, u') = splitChunkIntoRegion u c
-      return $ normaliseUniverse u == normaliseUniverse (fuseInhabitantRegions u' (regionParent u r))
+      return $ normaliseUniverse u === normaliseUniverse (fuseInhabitantRegions u' (regionParent u r))
