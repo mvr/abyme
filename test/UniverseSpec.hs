ce2020018f2d084938c8cef85aa2559e3dcbb610
Mitchell Riley <mitchell.v.riley@gmail.com>
2017-08-13 20:10:31 -0400

    Add basic test for splitChunkIntoRegion

--- test/UniverseSpec.hs
+++ test/UniverseSpec.hs
@@ -5,10 +5,20 @@ import Test.Hspec.QuickCheck
 
 import Abyme.Universe
 import Abyme.Universe.Generate
+import Abyme.Universe.Normalise
 import Abyme.Addressing
+import Abyme.Chunk
 
 spec :: Spec
-spec = describe "addressing" $ do
-        prop "square occupies its location" $ \u -> do
-          s <- randomSquare u
-          return $ Just s == inhabitant u (squareLocation u s)
+spec = do
+  describe "addressing" $ do
+    prop "square occupies its location" $ \u -> do
+      s <- randomSquare u
+      return $ Just s == inhabitant u (squareLocation u s)
+
+  describe "splitting" $ do
+    prop "splitting then unsplitting is identity" $ \u -> do
+      s <- randomPiece u
+      let c = findChunk u s
+      let (r, u') = splitChunkIntoRegion u c
+      return $ normaliseUniverse u == normaliseUniverse (fuseInhabitantRegions u' (regionParent u r))
