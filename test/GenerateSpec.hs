702468fffb1f5c8ccd684def533de1b9963692e7
Mitchell Riley <mitchell.v.riley@gmail.com>
2017-08-13 23:17:30 -0400

    Test that shrinking keeps universes valid

--- test/GenerateSpec.hs
+++ test/GenerateSpec.hs
@@ -2,6 +2,7 @@ module GenerateSpec (spec) where
 
 import Test.Hspec
 import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
 import Abyme.Universe
 import Abyme.Universe.Generate ()
@@ -11,3 +12,7 @@ spec :: Spec
 spec = describe "generator" $ do
         prop "generates valid universes" $ \u ->
           validateUniverse u
+
+        prop "shrinks universes to valid universes" $ \u ->
+          let shrinks = shrink u in
+          not (null shrinks) ==> forAll (elements shrinks) $ validateUniverse
