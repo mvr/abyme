6e97fd790c6e49e15f23583cbe3235b4b31b7b48
Mitchell Riley <mitchell.v.riley@gmail.com>
2017-08-30 18:17:56 -0400

    Validate that a region is connected

--- src/Abyme/Universe/Validate.hs
+++ src/Abyme/Universe/Validate.hs
@@ -3,6 +3,7 @@ module Abyme.Universe.Validate where
 import Control.Lens
 import Data.List (tails)
 
+import Abyme.Polyomino
 import Abyme.Universe
 import Abyme.Addressing
 
@@ -14,9 +15,10 @@ pairs :: [a] -> [(a, a)]
 pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
 
 validateRegion :: Universe -> Region -> Bool
-validateRegion u r@(Region rid pid _ ssh) = idsUnequal && shapesDontIntersect && everythingSitsOnSomething
+validateRegion u r@(Region rid pid _ ssh) = idsUnequal && shapesDontIntersect && regionIsConnected && everythingSitsOnSomething
   where idsUnequal = rid /= pid
         shapesDontIntersect = all (not . uncurry shapeIntersects) (pairs ssh)
+        regionIsConnected = polyIsConnected $ regionCompositePoly r
         everythingSitsOnSomething = all (\s -> Just s == inhabitant u (squareLocation u s)) (constituentSquares u r)
 
 validateUniverse :: Universe -> Bool
