6d412583dcad3aafb284ea1704eaea991602d611
Mitchell Riley <mitchell.v.riley@gmail.com>
2017-08-31 15:26:59 -0400

    Try hard to break ties between regions when normalising

--- src/Abyme/Universe/Normalise.hs
+++ src/Abyme/Universe/Normalise.hs
@@ -1,12 +1,13 @@
 module Abyme.Universe.Normalise where
 
 import Control.Lens
-import Data.List (sort, sortBy)
+import Data.List (sort, sortBy, (\\))
 import Data.Semigroup ((<>))
 import Linear
 
 import Abyme.Polyomino
 import Abyme.Universe
+import Abyme.Addressing
 
 -- This should be a normal form
 -- Uh oh, would I have to check graph isomorphism?
@@ -22,8 +23,8 @@ normaliseShape (Shape pos poly) = Shape (pos + origin) newpoly
   where (newpoly, origin) = normalisePoly poly
 
 -- Disregarding the id/parentid
-normaliseRegion :: Region -> Region
-normaliseRegion (Region rid pid pos shs) = Region rid pid (pos + origin) newshs
+normaliseRegionData :: Region -> Region
+normaliseRegionData (Region rid pid pos shs) = Region rid pid (pos + origin) newshs
   where normalisedshs = sort $ fmap normaliseShape shs
         origin = head normalisedshs ^. shapePosition
         newshs = fmap (\s -> s & shapePosition -~ origin) normalisedshs
@@ -32,9 +33,21 @@ compareRegionData :: Region -> Region -> Ordering
 compareRegionData (Region _ _ pos shs) (Region _ _ pos' shs')
   = (pos `compare` pos') <> (shs `compare` shs')
 
+-- A lot of this should short-circuit unless the universe is very symmetrical
+compareRegion' :: Universe -> [Region] -> [Region] -> Region -> Region -> Ordering
+compareRegion' u r1seen r2seen r1 r2 = (r1 `compareRegionData` r2)
+                                       <> (length r1Children `compare` length r2Children)
+                                       <> mconcat (fmap subcompare $ zip r1Children r2Children)
+  where r1Children = childRegions u r1 \\ r1seen
+        r2Children = childRegions u r2 \\ r2seen
+        subcompare (r1', r2') = compareRegion' u (r1:r1seen) (r2:r2seen) r1' r2'
+
+compareRegion :: Universe -> Region -> Region -> Ordering
+compareRegion u r1 r2 = compareRegion' u [] [] r1 r2
+
 normaliseUniverse :: Universe -> Universe
 normaliseUniverse u = remapIds idMapping u'
-  where u' = u & universeRegions . traverse %~ normaliseRegion
-        normalised = sortBy compareRegionData $ u' ^.. universeRegions . traverse
+  where u' = u & universeRegions . traverse %~ normaliseRegionData
+        normalised = sortBy (compareRegion u') $ u' ^.. universeRegions . traverse
         infIds = fmap RegionId [1..]
         idMapping = zip (normalised ^.. traverse . regionId) infIds
