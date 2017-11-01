165bc5b203ebf3fcb0571c21c5cd8a4a9f494a40
Mitchell Riley <mitchell.v.riley@gmail.com>
2017-10-31 23:28:03 -0400

    Draw a square

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[macro_use] extern crate gfx;
+#[macro_use]
+extern crate gfx;
 
 extern crate gfx_window_glutin;
 extern crate glutin;
@@ -12,45 +13,97 @@ pub type ColorFormat = gfx::format::Srgba8;
 pub type DepthFormat = gfx::format::DepthStencil;
 
 const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+const WHITE: [f32; 3] = [1.0, 1.0, 1.0];
+
+const SQUARE: &[Vertex] = &[
+    Vertex {
+        pos: [0.5, -0.5],
+        color: WHITE,
+    },
+    Vertex {
+        pos: [-0.5, -0.5],
+        color: WHITE,
+    },
+    Vertex {
+        pos: [-0.5, 0.5],
+        color: WHITE,
+    },
+    Vertex {
+        pos: [0.5, 0.5],
+        color: WHITE,
+    },
+];
+
+const INDICES: &[u16] = &[0, 1, 2, 2, 3, 0];
+
+gfx_defines! {
+    vertex Vertex {
+        pos: [f32; 2] = "a_Pos",
+        color: [f32; 3] = "a_Color",
+    }
+
+    pipeline pipe {
+        vbuf: gfx::VertexBuffer<Vertex> = (),
+        out: gfx::RenderTarget<ColorFormat> = "Target0",
+    }
+}
 
 pub fn main() {
     let mut events_loop = glutin::EventsLoop::new();
     let builder = glutin::WindowBuilder::new()
-        .with_title("Square Toy".to_string())
+        .with_title("Abyme".to_string())
         .with_dimensions(800, 800);
-    let context = glutin::ContextBuilder::new()
-        .with_vsync(true);
+    let context = glutin::ContextBuilder::new().with_vsync(true);
 
-    let (window, mut device, mut factory, mut main_color, mut main_depth) =
+    let (window, mut device, mut factory, main_color, mut main_depth) =
         gfx_glutin::init::<ColorFormat, DepthFormat>(builder, context, &events_loop);
 
     let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+    let pso = factory
+        .create_pipeline_simple(
+            include_bytes!(concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/src/shaders/shape_150.glslv"
+            )),
+            include_bytes!(concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/src/shaders/shape_150.glslf"
+            )),
+            pipe::new(),
+        )
+        .unwrap();
+    let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(SQUARE, INDICES);
+    let mut data = pipe::Data {
+        vbuf: vertex_buffer,
+        out: main_color,
+    };
 
     let mut running = true;
     while running {
-        encoder.clear(&main_color, BLACK);
+        encoder.clear(&data.out, BLACK);
+        encoder.draw(&slice, &pso, &data);
         encoder.flush(&mut device);
         window.swap_buffers().unwrap();
 
         events_loop.poll_events(|e| {
             use glutin::WindowEvent::*;
-            match e {
-                glutin::Event::WindowEvent{window_id: _, event} =>
+            if let glutin::Event::WindowEvent { event, .. } = e {
                 match event {
-                    KeyboardInput { input: glutin::KeyboardInput { virtual_keycode: Some(glutin::VirtualKeyCode::Escape), ..}, .. }
-                    | Closed
-                        => running = false,
+                    KeyboardInput {
+                        input: glutin::KeyboardInput {
+                            virtual_keycode: Some(glutin::VirtualKeyCode::Escape), ..
+                        },
+                        ..
+                    } |
+                    Closed => running = false,
                     Resized(_, _) => {
-                        gfx_glutin::update_views(&window, &mut main_color, &mut main_depth);
-                    },
+                        gfx_glutin::update_views(&window, &mut data.out, &mut main_depth);
+                    }
                     _ => (),
-                },
-                _ => (),
+                }
             }
         });
 
-
-        window.swap_buffers().unwrap();
         device.cleanup();
     }
 }
