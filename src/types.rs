f64566bb642c28ab4db6fc1f1dafe0bc5ee6bbe2
Mitchell Riley <mitchell.v.riley@gmail.com>
2018-02-24 18:37:54 -0500

    Some simple helpers converting between vector types

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,6 @@
-extern crate euclid;
+// extern crate euclid;
+
+use num::Integer;
 
 use euclid::*;
 
@@ -13,3 +15,40 @@ pub type ChildPoint = TypedPoint2D<i32, ChildSpace>;
 // pub type FVec = TypedVector<f32>;
 
 pub const zoom_scale: u32 = 2;
+
+pub mod vectors {
+    use types::*;
+
+    #[inline]
+    pub fn coerce_up(v: ChildVec) -> UVec {
+        UVec::new(v.x, v.y)
+    }
+
+    #[inline]
+    pub fn truncate_up(v: ChildVec) -> UVec {
+        UVec::new(
+            v.x.div_floor(&(zoom_scale as i32)),
+            v.y.div_floor(&(zoom_scale as i32)),
+        )
+    }
+
+    #[inline]
+    pub fn mod_up(v: ChildVec) -> ChildVec {
+        ChildVec::new(v.x % zoom_scale as i32, v.y % zoom_scale as i32)
+    }
+
+    #[inline]
+    pub fn split_up(v: ChildVec) -> (UVec, ChildVec) {
+        (truncate_up(v), mod_up(v))
+    }
+
+    #[inline]
+    pub fn shift_down(v: UVec) -> ChildVec {
+        ChildVec::new(v.x * (zoom_scale as i32), v.y * (zoom_scale as i32))
+    }
+
+    #[inline]
+    pub fn coerce_down(v: UVec) -> ChildVec {
+        ChildVec::new(v.x, v.y)
+    }
+}
