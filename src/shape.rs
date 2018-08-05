5149fb8d614c8bfd55586c26b15ed823f2f9f596
Mitchell Riley <mitchell.v.riley@gmail.com>
2018-08-05 15:32:05 -0400

    Fix unblocked to check whether any squares are occupied

--- src/shape.rs
+++ src/shape.rs
@@ -132,7 +132,10 @@ trait HasSquares {
     fn unblocked(&self, universe: &Universe, d: Direction) -> bool {
         self.constituent_locations(universe)
             .map(|l| l.nudge(universe, d))
-            .all(|o| o.is_some()) // This is doing a little more calculation than is necessary...
+            .all(|o| match o {
+                None => false,
+                Some(a) => a.is_unoccupied(universe),
+            }) // This is doing a little more calculation than is necessary...
     }
     //     fringe u d a = (filter (not . inhabits u a) justs, length allMaybes /= length justs)
     // where allMaybes = fmap (nudgeLocation u d) $ locations u a
@@ -339,6 +342,10 @@ impl Location {
 
     fn wrap_subposition(v: ChildVec) -> ChildVec {
         ChildVec::new(v.x % ZOOM_SCALE as i32, v.y % ZOOM_SCALE as i32)
+    }
+
+    pub fn is_unoccupied(&self, universe: &Universe) -> bool {
+        !self.inhabitant(universe).is_some()
     }
 }
 
@@ -771,19 +778,20 @@ impl MonotonePath {
 mod movement_tests {
     use super::*;
 
-    fn setup() -> LogicalState {
+    // TODO: replace this with loading from a toml
+    fn setup_1() -> LogicalState {
         let id1 = ShapeId(1);
         let id2 = ShapeId(2);
         let shape1 = Shape {
             id: id1,
-            parent_ids: btreemap!{ id2 => ChildPoint::new(1, 0) },
+            parent_ids: hashmap!{ id2 => ChildPoint::new(1, 0) },
             polyomino: Polyomino::monomino(),
             fill_color: [0.5, 0.5, 1.0],
             outline_color: [0.5, 0.5, 0.5],
         };
         let shape2 = Shape {
             id: id2,
-            parent_ids: btreemap!{ id1 => ChildPoint::new(0, 0) },
+            parent_ids: hashmap!{ id1 => ChildPoint::new(0, 0) },
             polyomino: Polyomino::monomino(),
             fill_color: [1.0, 0.5, 0.5],
             outline_color: [0.5, 0.25, 0.25],
@@ -805,7 +813,7 @@ mod movement_tests {
 
     #[test]
     fn inhabitant_1() {
-        let gs = setup();
+        let gs = setup_1();
 
         let l = Location {
             square: Square {
@@ -819,9 +827,57 @@ mod movement_tests {
     }
 
     #[test]
-    fn test_1() {
-        let gs = setup();
+    fn is_blocked_1() {
+        let gs = setup_1();
 
         assert!(!gs.can_move(Direction::Right));
+    }
+
+    fn setup_2() -> LogicalState {
+        let id1 = ShapeId(1);
+        let id2 = ShapeId(2);
+        let id3 = ShapeId(3);
+        let shape1 = Shape {
+            id: id1,
+            parent_ids: hashmap!{ id2 => ChildPoint::new(0, 0) },
+            polyomino: Polyomino::monomino(),
+            fill_color: [0.5, 0.5, 1.0],
+            outline_color: [0.5, 0.5, 0.5],
+        };
+        let shape2 = Shape {
+            id: id2,
+            parent_ids: hashmap!{ id1 => ChildPoint::new(0, 0) },
+            polyomino: Polyomino::monomino(),
+            fill_color: [1.0, 0.5, 0.5],
+            outline_color: [0.5, 0.25, 0.25],
+        };
+        let shape3 = Shape {
+            id: id3,
+            parent_ids: hashmap!{ id2 => ChildPoint::new(0, 1) },
+            polyomino: Polyomino::monomino(),
+            fill_color: [1.0, 0.5, 0.5],
+            outline_color: [0.5, 0.25, 0.25],
+        };
+
+        let mut shapes = BTreeMap::new();
+        shapes.insert(id1, shape1);
+        shapes.insert(id2, shape2);
+        shapes.insert(id3, shape3);
+
+        let u = Universe { shapes: shapes };
+
+        let player_chunk = u.top_chunk_of_id(ShapeId(1));
+
+        LogicalState {
+            universe: u,
+            player_chunk: player_chunk,
+        }
+    }
+
+    #[test]
+    fn is_blocked_2() {
+        let gs = setup_2();
+
+        assert!(!gs.can_move(Direction::Up));
     }
 }
